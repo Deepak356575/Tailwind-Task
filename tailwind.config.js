c3fba8972e79c0fe0e3460a0d5387f74d4ce0766
Deepak356575 <deepak356575@gmail.com>
2024-10-23 12:39:12 +0530

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        font1:['Righteous',],
+        font2:["Saira"],
+        font3:["Rowdies"],
+       },
+    },
+  },
+  plugins: [],
+}
